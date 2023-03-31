13b5f978173de6c4d55678b5d7c71b317f0eddce
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-03-31 10:03:18 -0700

    wip: configures use of custom fonts

--- app/_layout.js
+++ app/_layout.js
@@ -1,5 +1,29 @@
 import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from 'expo-splash-screen'
+
+// Makes the native splash screen visible until hideAsync is called
+SplashScreen.preventAutoHideAsync()
 
 export default function Layout() {
-    return <Stack />;
+
+    // import custom fonts using useFonts 
+    const [fontsLoaded] = useFonts({
+        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+        DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+    })
+
+    // Similar to useEffect
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            // only show the home page once the fonts have been loaded
+            await SplashScreen.hideAsync()
+        }
+    }, [fontsLoaded])
+
+    if(!fontsLoaded) return null
+
+    return <Stack onLayout={onLayoutRootView}/>;
 }
