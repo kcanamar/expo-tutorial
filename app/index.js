77e94279dab5fa930af9837eff976a19c681d121
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-04-01 19:33:18 -0700

    wip: company info dispay on each indiviual company

--- app/index.js
+++ app/index.js
@@ -33,9 +33,7 @@ export default function Home() {
                         padding: SIZES.medium
                     }}
                 >
-                 <Welcome
-
-                 />
+                 <Welcome/>
                  <Popularjobs />
                  <Nearbyjobs />
                 </View>
